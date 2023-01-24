7ea30135fd0b908f8247a7a8e8a84852501ddd39
*Diogo <diogolimakas@hotmail.com>
2023-01-24 18:27:36 -0300

    adição de categorias e removção de elementos não desejados

--- src/api/categoria/services/categoria.js
+++ src/api/categoria/services/categoria.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * post service
+ * categoria service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::post.post');
+module.exports = createCoreService('api::categoria.categoria');
